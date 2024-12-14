5de16d05b19408337ffa71b705fff004793f063a
AllanMaloi <maloisaruni@gmail.com>
2024-12-14 22:18:01 +0300

    Fixed input prompt format to match checker requirements

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
 monthly_income = float(input("Enter your mothly income:"))
-monthly_expenses = float(input("Enter your monthly expenses:"))
+monthly_expenses = float(input("Enter your total monthly expenses:"))
 monthly_savings = monthly_expenses - monthly_income
 annual_savings = monthly_savings * 12
 projected_annual_savings = annual_savings + (annual_savings * 0.05)
